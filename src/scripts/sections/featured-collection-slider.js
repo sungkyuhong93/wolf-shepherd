d851c2d8df3f802d94f9d76531860d756b69f344
Louie Babb <louie.babb@wemakewebsites.info>
2019-10-18 18:34:55 +0100

    Collection grid item

--- src/scripts/sections/featured-collection-slider.js
+++ src/scripts/sections/featured-collection-slider.js
@@ -3,36 +3,36 @@ import _ from 'slick-carousel';
 
 theme.CollectionSlider = (function() {
 
-	$('.collection-carousel').slick({
-		arrows:true,
-		vertical:false,
-		prevArrow: '<div class="slick-arrow slick-prev"> < </div>',
-		nextArrow: '<div class="slick-arrow slick-next"> > </div>',
-	  	responsive: [
-		    {
-		      breakpoint: 1024,
-		      settings: {
-		        slidesToShow: 3,
-		        slidesToScroll: 3,
-		        infinite: true,
-		        dots: true
-		      }
-		    },
-		    {
-		      breakpoint: 600,
-		      settings: {
-		        slidesToShow: 2,
-		        slidesToScroll: 2
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        slidesToShow: 1,
-		        slidesToScroll: 1
-		      }
-		    }
-	  	]
-	});
+  $('.collection-carousel').slick({
+    arrows: true,
+    vertical: false,
+    prevArrow: '<div class="slick-arrow slick-prev"> < </div>',
+    nextArrow: '<div class="slick-arrow slick-next"> > </div>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 
 })();
