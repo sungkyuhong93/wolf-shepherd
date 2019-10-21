1ee70b226dc3207a4e590b28acab2875dde10fdd
Louie Babb <louie.babb@wemakewebsites.info>
2019-10-21 13:33:23 +0100

    Collectio list and testimonials

--- src/scripts/sections/testimonials.js
+++ src/scripts/sections/testimonials.js
@@ -1,13 +1,37 @@
 import $ from 'jquery';
+import _ from 'slick-carousel';
+import {register} from '@shopify/theme-sections';
 
-theme.Testimonials = (function() {
+register('testimonials', {
+  onLoad() {
+    this.init();
+  },
 
-	$('.quotes-slider').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: true,
-		fade: true,
-		dots:true
-	});
+  init() {
+    this.slickRender();
+  },
 
-})();
+  slickRender() {
+    const $slideshow = $('.testimonials__slider');
+    $slideshow.slick(this.getSliderSettings());
+  },
+
+  getSliderSettings() {
+    return {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      fade: true,
+      dots: true,
+    };
+  },
+
+  onBlockSelect: (blockId) => { 
+    const $blockClass = $(`.testimonials__slide--${blockId}`).closest('.slick-slide');
+    const slideIndex  = $blockClass.attr('data-slick-index');
+    $('.testimonials__slider').slick('goTo', slideIndex);
+  },
+
+  onSelect: () => {
+  },
+});
