5cb81ff226ad4779b233042b783ed106f4fbba5e
Louie Babb <louie.babb@wemakewebsites.info>
2019-11-25 13:01:02 +0000

    Account/order toggle

---
+++ src/scripts/templates/customers/account.js
@@ -0,0 +1,86 @@
+/**
+ * Password Template Script
+ * ------------------------------------------------------------------------------
+ * A file that contains scripts highly couple code to the Password template.
+ *
+ * @namespace password
+ */
+
+import $ from 'jquery';
+
+const selectors = {
+  accountOrders: '#accountOrders',
+  showAccount: '#accountDetailsShow',
+  showOrders: '#accountOrdersShow',
+  orders: '#accountOrders',
+  account: '#accountDetails',
+};
+
+function checkUrlHash() {
+  const hash = window.location.hash;
+
+  // Allow deep linking to recover password form
+  if (hash === '#accountOrders') {
+    $(selectors.orders).addClass('is-active');
+    $(selectors.account).removeClass('is-active');
+    $(selectors.showOrders).addClass('active');
+    $(selectors.showAccount).removeClass('active');
+    $('#shopify-section-account-featured-collection').addClass('is-active');
+  } else {
+    $(selectors.account).addClass('is-active');
+    $(selectors.orders).removeClass('is-active');
+    $(selectors.showOrders).removeClass('active');
+    $(selectors.showAccount).addClass('active');    
+    $('#shopify-section-account-featured-collection').removeClass('is-active');
+  }
+}
+
+/**
+ *  Show orders
+ */
+function showOrders(evt) {
+  evt.preventDefault();
+  $(selectors.orders).addClass('is-active');
+  $(selectors.account).removeClass('is-active');
+  $(selectors.showOrders).addClass('active');
+  $(selectors.showAccount).removeClass('active');
+  $('#shopify-section-account-featured-collection').addClass('is-active');
+
+  addMobileClass();
+}
+
+/**
+ *  Show orders
+ */
+function showAccount(evt) {
+  evt.preventDefault();
+  $(selectors.account).addClass('is-active');
+  $(selectors.orders).removeClass('is-active');
+  $(selectors.showOrders).removeClass('active');
+  $(selectors.showAccount).addClass('active');
+  $('#shopify-section-account-featured-collection').removeClass('is-active');
+
+  addMobileClass();
+}
+
+/**
+ *  Hide nav
+ */
+function addMobileClass() {
+  $('body').addClass('account-nav-hide');
+}
+
+/**
+ *  Show Nav
+ */
+function showMobileNav() {
+  $('body').removeClass('account-nav-hide');
+}
+
+if ($(selectors.accountOrders).length) {
+  checkUrlHash();
+
+  $(selectors.showOrders).on('click', showOrders);
+  $(selectors.showAccount).on('click', showAccount);
+  $('.account__breadcrumb').on('click', showMobileNav)
+}
