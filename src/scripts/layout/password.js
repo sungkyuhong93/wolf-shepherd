ccec5294b3b602e09d1430cd7b656fadde27d6a9
Louie Babb <louie.babb@wemakewebsites.info>
2019-11-28 22:03:25 +0000

    Password fix, careers

--- src/scripts/layout/password.js
+++ src/scripts/layout/password.js
@@ -7,3 +7,16 @@
  */
 
 import '../../styles/templates/password.scss';
+
+import $ from 'jquery';
+
+
+/**
+ * Export default exitIntent.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('loas');
+  $('#login-open').on('click', function(){
+    $('#modal').addClass('is-active');
+  })
+});
