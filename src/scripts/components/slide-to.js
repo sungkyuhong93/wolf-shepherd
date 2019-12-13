655904c853e11027433f0f1f66f396c55e4f3352
Louie Babb <louie.babb@wemakewebsites.info>
2019-12-13 23:40:14 +0000

    Product reviews

--- src/scripts/components/slide-to.js
+++ src/scripts/components/slide-to.js
@@ -3,29 +3,14 @@
 import $ from 'jquery';
 
 theme.SlideTo = (function() {
-  $('.slide-to').click(function(ev) {
+  $('.slide-to').click(function(event) {
+    event.preventDefault();
     const navHeight = $('.site-header').innerHeight();
+    const slideLink = $(this).attr('data-slide');
 
-    // if ($(window).width() < 992) {
-    //   // code for small viewports
-    //   var navHeight = 65;
-    // } else if ($(window).width() < 1320) {
-    //   // code for large viewports
-    //   var navHeight = 71;
-    // } else {
-    //   var navHeight = 80;
-    // }
-
-    ev.preventDefault();
-
-    const slideLink = $(this).attr('href');
-
-    $('.slide-to').removeClass('active');
-    $(this).addClass('active');
     $('html, body').animate({
       scrollTop: $(slideLink).offset().top - navHeight
-    }, 1000);
-
+    }, 1500);
   });
 
 })();
